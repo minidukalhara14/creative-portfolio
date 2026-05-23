adbadfbc5f1ec538dca100c6c9588d6448c4d6bb
minidukalhara2@gmail.com <minidukalhara14@gmail.com>
2026-05-23 21:44:37 +0530

    initialize the  HeroCard 🚀

---
+++ src/components/common/HeroCard.jsx
@@ -0,0 +1,10 @@
+export default function HeroCard() {
+    return (
+        <div className="w-[calc(100vw-70px)] h-screen bg-secondary text-primary flex justify-center items-center relative ">
+            
+            <div className="w-full h-[calc(100%+45px)] absolute -top-[50px] bg-secondary flex justify-center items-center flex-col rounded-2xl">
+               
+            </div>
+        </div>
+    );
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,14 @@
 import Navibar from "../components/common/Navibar";
+
 import Hero from "../components/sections/Hero";
 
 export default function HomePage() {
     return (
-        <>
+        <div className="h-full w-screen flex  flex-col">
             
             <Hero />
-        </>
+            
+        
+        </div>
     );
 }
