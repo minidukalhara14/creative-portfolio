41b39e00864cbe6db64e2a2b83a4924900557834
minidukalhara2@gmail.com <minidukalhara14@gmail.com>
2026-06-15 23:41:21 +0530

    heros bug

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -21,7 +21,7 @@ export default function Hero() {
           className="
             w-full
             lg:h-[calc(100vh-80px)]
-            lg:-mt-[70px]
+            lg:mt-[70px]
             bg-blue-800
             pb-[50px] 
             md:pb-5
